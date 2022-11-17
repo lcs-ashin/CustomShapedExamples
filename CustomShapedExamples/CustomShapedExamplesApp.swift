8d3b501a194e952331840e6ce0fbde8581daa8e9
lcs-ashin <ashin@lcs.on.ca>
2022-11-17 11:42:01 -0500

    Initial Commit

---
+++ CustomShapedExamples/CustomShapedExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapedExamplesApp.swift
+//  CustomShapedExamples
+//
+//  Created by Eunbi Shin on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapedExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
